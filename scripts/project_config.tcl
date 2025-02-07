f4eda5d299b333b70e02d89334f59f437c77eb06
J <jswenke@outlook.com>
2025-02-06 23:24:01 -0500

    bin2gray converter, fixed build script IP gen part

--- scripts/project_config.tcl
+++ scripts/project_config.tcl
@@ -40,6 +40,10 @@ proc addHDL {} {
 }
 
 proc genIP {} {
+	
+	global PROJECT_NAME
+	global sourceDir
+
 	# no IP yet
 	create_ip -name blk_mem_gen -vendor xilinx.com -library ip -version 8.4 -module_name blk_mem_dram_0 -dir $sourceDir/rtl/xil_and_3rds_ip
 	set_property -dict [list CONFIG.Component_Name {blk_mem_dram_0} CONFIG.Interface_Type {Native} CONFIG.Use_AXI_ID {false} CONFIG.Memory_Type {Simple_Dual_Port_RAM} CONFIG.Use_Byte_Write_Enable {false} CONFIG.Byte_Size {9} CONFIG.Assume_Synchronous_Clk {true} CONFIG.Write_Width_A {32} CONFIG.Write_Depth_A {16} CONFIG.Read_Width_A {32} CONFIG.Operating_Mode_A {NO_CHANGE} CONFIG.Write_Width_B {32} CONFIG.Read_Width_B {32} CONFIG.Operating_Mode_B {READ_FIRST} CONFIG.Enable_B {Always_Enabled} CONFIG.Register_PortA_Output_of_Memory_Primitives {false} CONFIG.Register_PortB_Output_of_Memory_Primitives {true} CONFIG.Use_RSTB_Pin {false} CONFIG.Reset_Type {SYNC} CONFIG.Port_B_Clock {100} CONFIG.Port_B_Enable_Rate {100} CONFIG.EN_SAFETY_CKT {false}] [get_ips blk_mem_dram_0]
