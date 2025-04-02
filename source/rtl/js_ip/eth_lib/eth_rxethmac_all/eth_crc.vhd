442e37eb0577a2bcd2d65442f941caaf1c835449
J <jswenke@outlook.com>
2025-04-01 21:52:56 -0400

    Started CRC

---
+++ source/rtl/js_ip/eth_lib/eth_rxethmac_all/eth_crc.vhd
@@ -0,0 +1,36 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/01/2025 09:24:28 PM
+-- Design Name: 
+-- Module Name: eth_crc - rtl
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;   
+    use IEEE.STD_LOGIC_1164.ALL;
+    use IEEE.NUMERIC_STD.ALL;
+
+
+entity eth_crc is
+--  Port ( );
+end eth_crc;
+
+architecture rtl of eth_crc is
+
+begin
+
+
+end rtl;
